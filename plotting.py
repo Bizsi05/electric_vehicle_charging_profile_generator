5c3c0df7e55238226e561cc9866664d387436ca1
Bizsi05 <hevizibalazs2000@gmail.com>
2025-12-09 12:55:25 +0100

    Plotting the results of the simulations

--- plotting.py
+++ plotting.py
@@ -1,10 +1,8 @@
-import pickle
-from os.path import join, exists
-
+from config import DT_MINUTES, MARGINAL_SAMPLES
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.mixture import GaussianMixture
-from simulations import simulate_year
+
 
 
 def plot_gmm_marginal(gmm: GaussianMixture, dim=0,
@@ -15,7 +13,7 @@ def plot_gmm_marginal(gmm: GaussianMixture, dim=0,
     """
 
     # 1) sok minta generálása a keverékmodellből
-    X, _ = gmm.sample(50000)      # 50 000 minta (start time, energy)
+    X, _ = gmm.sample(MARGINAL_SAMPLES)      # 50 000 minta (start time, energy)
     data = X[:, dim]              # csak az adott dimenzió
     # ha az időt rajzoljuk (0. dimenzió), váltsuk át órára
     if dim == 0:
@@ -38,18 +36,18 @@ def plot_segment_profiles_one_day(profiles,title):
 
     plt.xlabel("Óra")
     plt.ylabel("Összteljesítmény [kW]")
-    plt.title("Weekday – szegmensenkénti töltési profil (1 nap)")
+    plt.title(title)
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
     plt.show()
 
-def plot_daily_energy_by_segment (yearly_profiles, year, dt_minutes):
+def plot_daily_energy_by_segment (yearly_profiles, year):
 
 
     # === NAPI ÖSSZENERGIA SZEGMENSENKÉNT, EGÉSZ ÉVRE ===
 
-    dt_h = dt_minutes / 60.0
+    dt_h = DT_MINUTES / 60.0
     steps_per_day = int(24 / dt_h)
 
     # Tegyük fel, hogy minden szegmensnél ugyanannyi lépés van (1 év)
@@ -81,7 +79,7 @@ def plot_daily_energy_by_segment (yearly_profiles, year, dt_minutes):
     plt.tight_layout()
     plt.show()
 
-def plot_mean_daily_profile_by_segment(yearly_profiles, year, dt_minutes):
+def plot_mean_daily_profile_by_segment(yearly_profiles, year):
     # 1) home szegmens teljes éves idősorának kirajzolása
     """
     home_profile = yearly_profiles.get("home")
@@ -97,8 +95,7 @@ def plot_mean_daily_profile_by_segment(yearly_profiles, year, dt_minutes):
 
     # 2) ÁTLAGOS NAPI PROFIL AZ EGÉSZ ÉVBŐL – MINDEN SZEGMENSRE
 
-    dt_minutes_year = 60
-    dt_h = dt_minutes_year / 60.0
+    dt_h = DT_MINUTES / 60.0
     steps_per_day = int(24 / dt_h)
     t_day = np.arange(0, 24, dt_h)
 
