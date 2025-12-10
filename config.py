c0e531217483ba1479bcaf8971c7b33d018b0fee
Bizsi05 <hevizibalazs2000@gmail.com>
2025-12-10 23:07:14 +0100

    Parameter setup

--- config.py
+++ config.py
@@ -3,6 +3,7 @@
 N_SAMPLES  = 50    # hány mintát húzunk GMM-enként naponta
 DT_MINUTES = 60    # időfelbontás percben (60 = órás rács)
 DURATION_H = 2.0   # feltételezett töltési idő egy sessionre [óra]
+CHARGING_POWER_KW = 6.6   # konstans töltési teljesítmény [kW] (állítható paraméter)
 YEAR=2021         # melyik év szimulációja
 MARGINAL_SAMPLES = 50000  # hány minta a marginal ábrákhoz
 SEGMENTS = ["home", "mud", "public", "work"] #szegmensek listája
