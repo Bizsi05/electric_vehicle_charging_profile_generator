1875ac1b9c344deb9a462691d26eb65b25228bd1
Bizsi05 <hevizibalazs2000@gmail.com>
2025-12-12 17:07:58 +0100

    Parameter setup

--- config.py
+++ config.py
@@ -6,12 +6,13 @@ DURATION_H = 2.0   # feltételezett töltési idő egy sessionre [óra]
 CHARGING_POWER_KW = 6.6   # konstans töltési teljesítmény [kW] (állítható paraméter)
 YEAR=2021         # melyik év szimulációja
 MARGINAL_SAMPLES = 50000  # hány minta a marginal ábrákhoz
+EV_COUNT = 584 #Hány EV töltést szimulálunk összesen egy nap alatt
 SEGMENTS = ["home", "mud", "public", "work"] #szegmensek listája
 ROOT=r'C:/Users/heviz/Documents/Egyetem/Msc/Önlab 2/SPEECh Model for Study on Grid Impacts of Charging Infrastructure Access/SPEECh Model for Study on Grid Impacts of Charging Infrastructure Access/ChargingModelData/GMMs'
 # ---------------------------------
 
 # ---- MEKH INTEGRÁCIÓ ----
-USE_MEKH_DATA   = False   # ha True, akkor skáláz a MEKH átlagok alapján
+USE_MEKH_DATA   = True   # ha True, akkor skáláz a MEKH átlagok alapján
 MEKH_EXCEL_PATH = r"C:\Users\heviz\Documents\Egyetem\Msc\Önlab 2\Avg_energy_used_for_charging_Hungary.xlsx"   # AZ EXCEL ÚTJA
-MEKH_COUNTY     = "Budapest"   # melyik megye sorát használjuk
+MEKH_COUNTY     = "Heves"   # melyik megye sorát használjuk
 # -------------------------
